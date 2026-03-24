577fc3d4efab6bff92dd13c4063ff0c5abd5e833
Rich-10-afk <richiemwangi19@gmail.com>
2026-03-24 19:50:14 +0300

    Initial Commit

---
+++ HomeGuard/HomeGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeGuardApp.swift
+//  HomeGuard
+//
+//  Created by Ryan Kibet on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HomeGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
